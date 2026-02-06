9106aa500f97e4f4eee478408bb027fc1c04086c
joelp2002 <parino.joel@marsu.edu.ph>
2026-02-06 10:22:21 +0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const dns = require('dns');
 const mongoose = require('mongoose');
 
-// Use reliable public DNS servers for c-ares (Node's DNS resolver)
-// This helps when the system resolver blocks SRV lookups used by mongodb+srv
+//public DNS servers for c-ares (Node's DNS resolver)
+//This helps when the system resolver blocks SRV lookups used by mongodb+srv
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
